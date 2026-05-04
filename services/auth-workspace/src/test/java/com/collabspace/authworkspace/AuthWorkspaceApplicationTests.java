aa1e9425cf6b774df99268efa69febe222d6d9d3
fejes99 <david.fejes@gmail.com>
2026-05-04 17:48:50 +0200

    Scaffold auth-workspace Spring Boot skeleton

---
+++ services/auth-workspace/src/test/java/com/collabspace/authworkspace/AuthWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabspace.authworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
