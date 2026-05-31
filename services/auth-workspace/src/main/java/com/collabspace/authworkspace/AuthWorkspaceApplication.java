282ee8fe62c5fe87f0caec29cd6a0ad0a94c6140
davidfejes <david.fejes@gmail.com>
2026-05-31 21:37:50 +0200

    Add dependencies

--- services/auth-workspace/src/main/java/com/collabspace/authworkspace/AuthWorkspaceApplication.java
+++ services/auth-workspace/src/main/java/com/collabspace/authworkspace/AuthWorkspaceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthWorkspaceApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(AuthWorkspaceApplication.class, args);
 	}
 
